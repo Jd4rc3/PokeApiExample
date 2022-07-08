fb180b37731f9bbc37d1ef2f807a71074c4d9bd0
Jd4rc3 <jdarce91@misena.edu.co>
2022-07-08 17:12:43 -0500

    Implemented okHttpClient insted java's httpClient

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,18 @@
 package org.example;
 
+import com.google.gson.Gson;
+import org.jboss.logging.Logger;
+
 import java.io.IOException;
-import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) throws URISyntaxException, IOException,
-            InterruptedException {
+    public static void main(String[] args) throws IOException {
+        Gson gson = new Gson();
+        var client = new HttpClientWrapper();
+        Logger log = Logger.getLogger(Main.class);
 
-        new HttpCustomClient().run();
+        Pokemon bulbasaur = gson.fromJson(client.run("1"), Pokemon.class);
+
+        log.info(bulbasaur.toString());
     }
 }
